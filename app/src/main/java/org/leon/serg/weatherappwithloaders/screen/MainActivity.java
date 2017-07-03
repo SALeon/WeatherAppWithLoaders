c0f604927e5bccd009f9d43e298bb5cedb74ced3
SALeon <developleon@gmail.com>
2017-07-03 23:51:20 +0300

    Initial commit

---
+++ app/src/main/java/org/leon/serg/weatherappwithloaders/screen/MainActivity.java
@@ -0,0 +1,15 @@
+package org.leon.serg.weatherappwithloaders.screen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.leon.serg.weatherappwithloaders.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
